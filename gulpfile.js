cbcfa4ba17b229b69db1ac92bdc12396d9f1fa5e
ves-sev <newswecretword@yandex.ru>
2020-08-27 22:11:31 +0300

    fonts added, css for header and banner started

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,83 @@
 let gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    del = require('del'),
-    autoprefixer = require('gulp-autoprefixer');
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  del = require('del'),
+  autoprefixer = require('gulp-autoprefixer');
 
 
-gulp.task('clean', async function(){
+gulp.task('clean', async function () {
   del.sync('dist')
 })
 
-gulp.task('scss', function(){
+gulp.task('scss', function () {
   return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(autoprefixer({
-      browsers: ['last 8 versions']
+    .pipe(sass({
+      outputStyle: 'compressed'
     }))
-     .pipe(rename({suffix: '.min'}))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 8 versions'] // заменен ключ на overrideBrowserslist, ибо в терминале предупреждалки разные
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('css', function(){
+gulp.task('css', function () {
   return gulp.src([
-    'node_modules/normalize.css/normalize.css',
-    'node_modules/slick-carousel/slick/slick.css',
-    'node_modules/rateyo/lib/cjs/rateyo.css',
-  ])
+      'node_modules/normalize.css/normalize.css',
+      'node_modules/slick-carousel/slick/slick.css',
+      'node_modules/rateyo/lib/cjs/rateyo.css',
+    ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('html', function(){
+gulp.task('html', function () {
   return gulp.src('app/*.html')
-  .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('script', function(){
+gulp.task('script', function () {
   return gulp.src('app/js/*.js')
-  .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('js', function(){
+gulp.task('js', function () {
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/rateyo/lib/cjs/rateyo.js',
-  ])
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/rateyo/lib/cjs/rateyo.js',
+    ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('app/js'))
-    .pipe(browserSync.reload({stream: true}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   browserSync.init({
-      server: {
-          baseDir: "app/"
-      }
+    server: {
+      baseDir: "app/"
+    }
   });
 });
 
-gulp.task('export', function(){
+gulp.task('export', function () {
   let buildHtml = gulp.src('app/**/*.html')
     .pipe(gulp.dest('dist'));
 
@@ -72,15 +86,15 @@ gulp.task('export', function(){
 
   let BuildJs = gulp.src('app/js/**/*.js')
     .pipe(gulp.dest('dist/js'));
-    
+
   let BuildFonts = gulp.src('app/fonts/**/*.*')
     .pipe(gulp.dest('dist/fonts'));
 
   let BuildImg = gulp.src('app/img/**/*.*')
-    .pipe(gulp.dest('dist/img'));   
+    .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
   gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
   gulp.watch('app/*.html', gulp.parallel('html'))
   gulp.watch('app/js/*.js', gulp.parallel('script'))
@@ -88,4 +102,4 @@ gulp.task('watch', function(){
 
 gulp.task('build', gulp.series('clean', 'export'))
 
-gulp.task('default', gulp.parallel('css' ,'scss', 'js', 'browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('css', 'scss', 'js', 'browser-sync', 'watch'));
